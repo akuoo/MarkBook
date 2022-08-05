6dd29adc92552b2117cffa7f07ba3991d1b86080
GitHub Action <action@github.com>
2022-08-05 07:23:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/akuoo/markbook/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/akuoo/markbook/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.akuoo.markbook.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.akuoo.markbook.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/akuoo/markbook/services/MyProjectService.kt
+++ src/main/kotlin/com/github/akuoo/markbook/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.akuoo.markbook.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.akuoo.markbook.MyBundle
 
 class MyProjectService(project: Project) {
 
